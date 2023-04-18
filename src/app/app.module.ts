da2ba12a11e72a143b817075bdc67abc50c8e886
AndrePonce3322 <andreponce417@gmail.com>
2023-04-18 13:56:54 -0600

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { QRCODEComponent } from './qrcode/qrcode.component'
+import { RoutingModule } from './routing/routing.module';
+import { MusicComponent } from './music/music.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QRCODEComponent,
+    MusicComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
